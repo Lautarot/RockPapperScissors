a8e486c771389bb0132956fdccf8a7d0307b6fbb
Lautaro Turati <lautaroturati@outlook.com>
2020-01-04 02:10:45 -0300

    seteando sfml en el proyecto

--- RockPapperScissors/main.cpp
+++ RockPapperScissors/main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 using namespace std;
+using namespace sf;
 
 int main()
 {
+    RenderWindow window(VideoMode(800, 600), "Rock Papper & Scissors");
 
-	cout << "Hola" << endl;
-	return 0;
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
